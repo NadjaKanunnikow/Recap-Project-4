9f88072814afa6435fd05ae002ff2744c8ac784e
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-08-30 12:32:06 +0200

    finish handleSubmit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,26 @@
-export default function Form() {
+export default function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target[0].value;
+    console.log("Name:", name);
+    const isForGoodWeather = event.target[1].checked;
+    console.log("A new Activity was submitted:", isForGoodWeather);
+
+    const activityObject = { name: name, isForGoodWeather: isForGoodWeather };
+  }
+
   return (
     <div>
-      <header className="App-header">Add new Activity:</header>
-      <form className="App">
+      <form onSubmit={handleSubmit} className="App">
+        <h2 className="App-header">Add new Activity:</h2>
         <label aria-labelledby="name" htmlFor="name">
           Name:
         </label>
         <input type="text" id="name" name="name"></input>
-        <label aria-labelledby="checkbox" htmlFor="checkbox">
+        <label aria-labelledby="isForGoodWeather" htmlFor="checkbox">
           Good-weather activity:
         </label>
-        <input name="checkbox" id="checkbox" type="checkbox"></input>
+        <input name="checkbox" id="isForGoodWeather" type="checkbox"></input>
         <button type="submit">Submit</button>
       </form>
     </div>
