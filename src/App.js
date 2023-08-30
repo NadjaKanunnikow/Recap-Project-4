bfa2900b9ea652defc51dd0c1a746b5f5db2c78a
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2023-08-30 13:32:15 +0200

    try to call onAddActivity - unsuccessful

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 
 function App() {
+  // const [activity, setActivity]=useState("");
+  // setActivity(activityObject);
   return <Form />;
 }
 
